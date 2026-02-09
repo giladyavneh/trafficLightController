c26db8732b6f52ae69c825fdb2079fce00b27ea2
giladyavneh <67857522+giladyavneh@users.noreply.github.com>
2026-02-10 00:30:25 +0200

    sort photos

--- utils/slideshow.py
+++ utils/slideshow.py
@@ -1,16 +1,10 @@
 import cv2
 import os
-import time
 
 
 # Supported image extensions
 EXTENSIONS = (".jpg", ".jpeg", ".png", ".bmp")
 
-
-
-
-
-cv2.destroyAllWindows()
 
 def show_images_in_dir(dirname: str, delay: int = 150):
     images = [
@@ -26,13 +20,14 @@ def show_images_in_dir(dirname: str, delay: int = 150):
             continue
 
         # attach filename to the image
-        cv2.putText(img, dirname + str(i), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+        cv2.putText(img, dirname + '/' + img_name.split('_png')[0], (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
         cv2.imshow("Dataset Viewer", img)
         
         # 0.3 seconds = 300 ms
         if cv2.waitKey(delay) & 0xFF == 27:  # ESC to exit early
             break
-        
-show_images_in_dir("../kaggle_data/test/images", 100)
-show_images_in_dir("../kaggle_data/train/images", 100)
-show_images_in_dir("../kaggle_data/valid/images", 100)
+
+if __name__ == "__main__":
+    show_images_in_dir("../kaggle_data/test/images", 100)
+    show_images_in_dir("../kaggle_data/train/images", 100)
+    show_images_in_dir("../kaggle_data/valid/images", 100)
