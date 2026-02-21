44820651b0d506d7c1296f85c48377ee29c8eb9e
giladyavneh <67857522+giladyavneh@users.noreply.github.com>
2026-02-21 11:26:00 +0200

    Update benchmark.py

--- infra/benchmark.py
+++ infra/benchmark.py
@@ -2,10 +2,10 @@ import torch
 
 if torch.cuda.is_available():
     print("CUDA detected → Running GPU benchmark\n")
-    from benchmark_gpu import main
+    from infra.benchmark_gpu import main
 else:
     print("No CUDA → Running CPU benchmark\n")
-    from benchmark_cpu import main
+    from infra.benchmark_cpu import main
 
 if __name__ == "__main__":
     main()
